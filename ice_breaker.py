6c1c96c830c44cd8ce7558142a9e9e8afa40b8f0
Eden Marco <emarco177@gmail.com>
2024-04-13 20:47:54 +0300

    added output parsers

--- ice_breaker.py
+++ ice_breaker.py
@@ -3,6 +3,7 @@ from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
 
+from output_parsers import summary_parser
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
@@ -11,10 +12,12 @@ from third_parties.twitter import scrape_user_tweets
 
 def ice_break_with(name: str) -> str:
     linkedin_username = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url=linkedin_username, mock=True
+    )
 
     twitter_username = twitter_lookup_agent(name=name)
-    tweets = scrape_user_tweets(username=twitter_username)
+    tweets = scrape_user_tweets(username=twitter_username, mock=True)
 
     summary_template = """
     given the information about a person from linkedin {information},
@@ -23,14 +26,19 @@ def ice_break_with(name: str) -> str:
     2. two interesting facts about them 
 
     Use both information from twitter and Linkedin
+    \n{format_instructions}
     """
     summary_prompt_template = PromptTemplate(
-        input_variables=["information", "twitter_posts"], template=summary_template
+        input_variables=["information", "twitter_posts"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": summary_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    chain = summary_prompt_template | llm | summary_parser
 
     res = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
 
