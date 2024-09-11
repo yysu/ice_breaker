b8dbd7478ad8808cb69443b9418ed66dda9c97be
Eden Marco <emarco177@gmail.com>
2024-09-11 11:18:44 +0300

    updated to LCEL

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain.chains import LLMChain
 
 from third_parties.linkedin import scrape_linkedin_profile
 
@@ -22,7 +21,7 @@ if __name__ == "__main__":
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    chain = summary_prompt_template | llm
     linkedin_data = scrape_linkedin_profile(
         linkedin_profile_url="https://www.linkedin.com/in/eden-marco/"
     )
