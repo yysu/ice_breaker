653a968efafaaadada9fe315af9a787d4ae972be
Eden Marco <emarco177@gmail.com>
2024-10-04 19:47:17 +0300

    model updates

--- agents/twitter_lookup_agent.py
+++ agents/twitter_lookup_agent.py
@@ -15,7 +15,7 @@ from tools.tools import get_profile_url_tavily
 def lookup(name: str) -> str:
     llm = ChatOpenAI(
         temperature=0,
-        model_name="gpt-3.5-turbo",
+        model_name="gpt-4o-mini",
     )
     template = """
        given the name {name_of_person} I want you to find a link to their Twitter profile page, and extract from it their username
--- ice_breaker.py
+++ ice_breaker.py
@@ -35,7 +35,7 @@ def ice_break_with(name: str) -> str:
         },
     )
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
 
     chain = summary_prompt_template | llm | summary_parser
 
